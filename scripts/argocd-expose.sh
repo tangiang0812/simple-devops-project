97eee54d77665537415909d36341d99bdf746835
Giang Nguyen <tangiang0812@gmail.com>
2026-04-05 00:49:09 +0700

    script: update argocd scripts

--- scripts/argocd-expose.sh
+++ scripts/argocd-expose.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # kubectl port-forward svc/argocd-server -n argocd 8080:443 &
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-echo
-echo "---- ArgoCD admin secret ----"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-echo
-echo "---- ArgoCD admin secret ----"
--- scripts/argocd-install.sh
+++ scripts/argocd-install.sh
@@ -3,5 +3,11 @@
 kubectl create namespace argocd
 kubectl apply --server-side --force-conflicts -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # kubectl rollout restart deployment -n argocd
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+kubectl annotate svc argocd-server -n argocd \
+  service.beta.kubernetes.io/aws-load-balancer-name=argocd-lb \
+  service.beta.kubernetes.io/aws-load-balancer-additional-resource-tags="Name=argocd-lb,Environment=production,Project=argocd" \
+  --overwrite
 echo
